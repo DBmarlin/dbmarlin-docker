3b73ecf203c613bd254dcfc291d732dcd95293d9
Mick McGuinness <mick.mcguinness@applicationperformance.com>
2024-02-08 20:28:04 +0000

    start-and-tail.sh

---
+++ docker/agent-docker/start-and-tail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pwd
+whoami
+ls -l
+
+# Start DBmarlin agent in the background
+/opt/dbmarlin/agent-start.sh &
+
+# Wait a bit for the app to initialize and logs to be created if needed
+sleep 5
+
+# Use a while loop to continuously check for new log files and tail them
+# This is a simplistic approach; consider enhancing for production use
+while true; do
+  tail -n 100 -F /opt/dbmarlin/tomcat/logs/localhost.$(date +%Y-%m-%d).log
+  sleep 60 # Check for a new file every 60 seconds
+done
